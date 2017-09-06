87ce31fa9897a79cc50240bc14da23d5b48d92cb
ebenvy <ebenvy@naver.com>
2017-09-06 17:53:51 +0900

    큐예제- 스트링

--- queue_example_string.c
+++ queue_example_string.c
@@ -17,8 +17,9 @@ typedef struct PERSON
 }PERSON;
 static void _free_fun(gpointer data)
 {
-	//if(data)
-		//free(data);
+	printf("free_fun\n");
+	if(data)
+		free(data);
 }
 
 
@@ -36,17 +37,32 @@ int main()
 	setvbuf(stderr, NULL, _IONBF, 0);
 
 	 GQueue* q = g_queue_new();
+	// char buf[1024]={0,};
+	 char* buf;
+	 buf= malloc(strlen("Misa"));
+	 sprintf(buf,"%s","Misa");
+	 g_queue_push_tail(q, buf);
+	 buf= malloc(strlen("Binna"));
+	 sprintf(buf,"%s","Binna");
 
-	 g_queue_push_tail(q, "Misa");
-	 g_queue_push_tail(q, "Binna");
-	 g_queue_push_tail(q, "Jeon");
-	 g_queue_push_tail(q, "Lim");
+	 g_queue_push_tail(q, buf);
+	 buf= malloc(strlen("Jeon"));
+	 sprintf(buf,"%s","Jeon");
+
+	 g_queue_push_tail(q, buf);
+
+	 buf= malloc(strlen("Lim"));
+	 sprintf(buf,"%s","Lim");
+
+	 g_queue_push_tail(q, buf);
 
 	 printf("Original queue: \n");
 	 g_queue_foreach(q, (GFunc)printPerson, NULL);
 	 g_queue_sort(q, (GCompareDataFunc)sorter, NULL);
 	 printf("Sorted queue: \n");
 	 g_queue_foreach(q, (GFunc)printPerson, NULL);
+	 printf("%s\n",(char*)g_queue_pop_head(q));
+
 	 g_queue_free_full(q,_free_fun);
 
 
